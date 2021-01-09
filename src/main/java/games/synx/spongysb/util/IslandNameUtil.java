04464d9974b2ca1dce833be5c9869bf6f255f78c
FawksX <xfawks@gmail.com>
2021-01-09 13:03:59 +0000

    make UUIDUtil and IslandNameUtil use IslandCache

--- src/main/java/games/synx/spongysb/util/IslandNameUtil.java
+++ src/main/java/games/synx/spongysb/util/IslandNameUtil.java
@@ -1,34 +1,18 @@
 package games.synx.spongysb.util;
 
-import games.synx.spongysb.SpongySB;
-import games.synx.spongysb.storage.DatabaseManager;
-import games.synx.spongysb.storage.Statements;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import games.synx.spongysb.cache.IslandCache;
+import games.synx.spongysb.objects.Island;
 
 public class IslandNameUtil {
 
   public static boolean isIslandNameTaken(String name) {
 
-    try(Connection connection = DatabaseManager.get().getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(Statements.GET_ISLAND_UUID)) {
-
-      preparedStatement.setString(1, name.toUpperCase());
-      ResultSet rs = preparedStatement.executeQuery();
-
-      if(rs.next()) {
+    for(Island island : IslandCache.getAll().values()) {
+      if(island.getIslandName().equals(name)) {
         return true;
       }
-
-      return false;
-    } catch (SQLException e) {
-      // Returning true here so that the island name is not accidently duplicated
-      e.printStackTrace();
-      return true;
     }
+    return false;
   }
 
 }
--- src/main/java/games/synx/spongysb/util/UUIDUtil.java
+++ src/main/java/games/synx/spongysb/util/UUIDUtil.java
@@ -1,13 +1,8 @@
 package games.synx.spongysb.util;
 
-import games.synx.spongysb.SpongySB;
-import games.synx.spongysb.storage.DatabaseManager;
-import games.synx.spongysb.storage.Statements;
+import games.synx.spongysb.cache.IslandCache;
+import games.synx.spongysb.objects.Island;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.UUID;
 
 public class UUIDUtil {
@@ -15,24 +10,12 @@ public class UUIDUtil {
   public static UUID generateUniqueIslandUUID() {
 
     UUID uuid = UUID.randomUUID();
-
-    try (Connection connection = DatabaseManager.get().getConnection()) {
-
-      PreparedStatement preparedStatement = connection.prepareStatement(Statements.GET_ISLAND);
-      preparedStatement.setString(1, uuid.toString());
-      ResultSet rs = preparedStatement.executeQuery();
-
-      if(!rs.next()) {
-        return uuid;
-      } else {
+    for(Island island : IslandCache.getAll().values()) {
+      if(island.getIslandUUID().toString().equals(uuid.toString())) {
         return generateUniqueIslandUUID();
       }
-
-    } catch (SQLException e) {
-      e.printStackTrace();
     }
-
-    throw new NullPointerException();
+    return uuid;
 
   }
 
